0db290942168ba5dc96e439e71a5b98d79c3f901
yu.guo <yu.guo@chinacache.com>
2014-03-30 13:14:59 +0800

    给订单添加付款类型，付款ID

--- apps/order/models/order.py
+++ apps/order/models/order.py
@@ -8,9 +8,12 @@ import datetime
 #城市
 class Orders(models.Model):
     STATUS = ((0,u'未付款'),(1,'已付款'),(2,'订单完成'),(3,'标记删除'))
+    PAY_TYPE = ((0,u'银联'),(1,u'支付宝'),(2,u'财付通'))
     oid = models.AutoField('订单号', primary_key=True, help_text='订单号', default=1000)
     user = models.ForeignKey(User)
     name = models.CharField(u'名称', max_length=254, help_text="名称", db_index=True)
+    pay_type = models.IntegerField(u'付款方式', max_length=2, help_text=u'付款方式', default=1)
+    pay_id = models.CharField(u'订单号', max_length=254, help_text="订单号")
     status = models.IntegerField(u'订单状态', max_length=2, help_text=u'订单状态', default=0, db_index=True)
     product = models.IntegerField(u'所属产品', max_length=10, help_text=u'所属产品')
     price = models.FloatField(u'价钱', max_length=64, help_text=u'价钱')
